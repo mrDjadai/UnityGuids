077a66071dd839ef09244bcf9666e6824918a0e0
mrDjadai <igor24122006@gmail.com>
2025-10-20 19:26:51 +0300

    Guid

---
+++ Scripts/Menu3D/PhysicalButton.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.Events;
+
+public class PhysicalButton : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onClick;
+    [SerializeField] private float animationDuration;
+    [SerializeField] private Transform movable;
+    [SerializeField] private Vector3 targetLocalPos;
+    private bool isAnimating;
+
+    private void OnMouseUpAsButton()
+    {
+        if (isAnimating)
+        {
+            return;
+        }
+        isAnimating = true;
+        movable.DOLocalMove(targetLocalPos, animationDuration / 2).OnComplete(OnComplete);
+    }
+
+    private void OnComplete()
+    {
+        onClick.Invoke();
+        movable.DOLocalMove(Vector3.zero, animationDuration / 2).OnComplete(End);
+    }
+
+    private void End()
+    {
+        isAnimating = false;
+    }
+}
